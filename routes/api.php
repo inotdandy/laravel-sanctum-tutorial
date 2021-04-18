86fc15ebc21d6f773d5b2871243d0c9eccdb010c
Dandy Inot <dandy.inot1990@gmail.com>
2021-04-18 22:34:57 +0800

    Install laravel sanctum, Add santum middleware on api route

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
-Route::put('/products/{product}', [ProductController::class, 'update']);
-Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+});
