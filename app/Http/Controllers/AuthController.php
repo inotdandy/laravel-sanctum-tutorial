873eb9292c044f58f3612f78035237711a61fa2d
Dandy Inot <dandy.inot1990@gmail.com>
2021-04-19 16:42:23 +0800

    Sanctum functionality, Create Register, login, logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|string',
+            'password' => 'required|string'
+
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+        
+        $token = $user->createToken('userToken')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'userToken' => $token
+        ]);
+
+    }
+
+    public function login(Request $request){
+
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            
+            return response()->json(['message' => 'Invalid credentials']);
+        }
+
+
+        $token = Auth::user()->createToken('userToken')->plainTextToken;
+
+        return response()->json([
+            'user' => Auth::user(),
+            'userToken' => $token
+        ]);
+    }
+
+    public function logout(){
+
+        Auth()->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logout']);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 /*
@@ -22,12 +23,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
-Route::get('/products/search/{name}', [ProductController::class, 'search']);
+//Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::group(['middleware' => 'auth:sanctum'], function(){
 
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{product}', [ProductController::class, 'update']);
-    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
 });
